f60fec20cf97adf53764fe2a72756c88a84f69bc
Cristian Ordonez <cristianordonezrd@gmail.com>
2026-08-15 14:45:27 -0400

    removed vcs for build

--- src/ntk/__init__.py
+++ src/ntk/__init__.py
@@ -2,6 +2,8 @@
 
 from __future__ import annotations
 
-from ._version import __version__ as VERSION  # noqa: N812
+from importlib.metadata import version
 
-__all__: list[str] = ["VERSION"]
+__version__ = version("my-package")
+
+__all__: list[str] = ["__version__"]
