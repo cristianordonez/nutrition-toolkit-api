023f8ece490d9149014ca47e5b168749f15d6465
Cristian Ordonez <cristianordonezrd@gmail.com>
2026-08-15 14:49:14 -0400

    updated version

--- src/ntk/__init__.py
+++ src/ntk/__init__.py
@@ -4,6 +4,6 @@ from __future__ import annotations
 
 from importlib.metadata import version
 
-__version__ = version("my-package")
+__version__ = version("ntk")
 
 __all__: list[str] = ["__version__"]
